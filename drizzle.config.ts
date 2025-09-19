7f7d894f7ecc7f561b076b715943fc448b62f598
akbarsahata <11588611+akbarsahata@users.noreply.github.com>
2025-09-20 05:57:07 +0700

    feat: configure dotenv and add job table schema with types

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,10 @@
-import { defineConfig } from 'drizzle-kit';
+import * as dotenv from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+dotenv.config({
+  path: ".env.local",
+});
+
 
 export default defineConfig({
   schema: './src/server/schema.ts',
---
+++ drizzle/0000_yellow_black_tom.sql
@@ -0,0 +1,12 @@
+CREATE TYPE "public"."job_type" AS ENUM('Full-Time', 'Part-Time', 'Contract');--> statement-breakpoint
+CREATE TABLE "jobs" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"title" text NOT NULL,
+	"company" text NOT NULL,
+	"description" text NOT NULL,
+	"location" text NOT NULL,
+	"type" "job_type" NOT NULL,
+	"user_id" uuid NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
